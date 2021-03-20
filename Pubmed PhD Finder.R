c76112aaccb1db1b3bfe4d5079b88f876d92815f
nf-exphys <68116178+nf-exphys@users.noreply.github.com>
2021-03-20 14:12:41 -0500

    Update Pubmed PhD Finder.R

--- Pubmed PhD Finder.R
+++ Pubmed PhD Finder.R
@@ -2,12 +2,16 @@ library(easyPubMed)
 
 my_query <- 'Damiano Fantini[AU] AND "2018"[PDAT]'
 my_entrez_id <- get_pubmed_ids(my_query)
+
+#extract with text format
 my_abstracts_txt <- fetch_pubmed_data(my_entrez_id, format = "abstract")
 head(my_abstracts_txt)
 
+#extract with txt format
 my_abstracts_xml <- fetch_pubmed_data(pubmed_id_list = my_entrez_id)
 class(my_abstracts_xml)
 
+#look for article title
 my_titles <- custom_grep(my_abstracts_xml, "ArticleTitle", "char")
 
 # use gsub to remove the tag, also trim long titles
@@ -17,6 +21,34 @@ my_titles[TTM] <- paste(substr(my_titles[TTM], 1, 70), "...", sep = "")
 # Print as a data.frame (use kable)
 head(my_titles)
 
-## Analyzing PubMed records ##
+
+#download records as txt
+out.A <- batch_pubmed_download(pubmed_query_string = my_query, 
+                               format = "txt", 
+                               batch_size = 20,
+                               dest_file_prefix = "easyPM_example",
+                               encoding = "ASCII")
+
+
+#### Analyzing PubMed records ####
 my_PM_list <- articles_to_list(pubmed_data = my_abstracts_xml)
 class(my_PM_list[1])
+
+#extracting fields of interest using custom_grep function
+curr_PM_record <- my_PM_list[1]
+custom_grep(curr_PM_record, tag = "PubDate")
+
+my.df <- article_to_df(curr_PM_record)
+colnames(my.df)
+
+custom_grep(curr_PM_record, tag = "Affiliation")
+
+
+#### Making it work for me ####
+
+my_query <- 'Damiano Fantini[AU] AND "2018"[PDAT]'
+my_entrez_id <- get_pubmed_ids(my_query)
+my_abstracts_xml <- fetch_pubmed_data(pubmed_id_list = my_entrez_id)
+curr_PM_record <- my_PM_list[1]
+my_PM_list <- articles_to_list(pubmed_data = my_abstracts_xml)
+custom_grep(curr_PM_record, tag = "Affiliation")
